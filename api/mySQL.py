1d2b5aafbb84c4f146ef1eb452425282a80b51f9
Hui Hwoo <hui.hwoo@gmail.com>
2023-05-20 17:04:28 -0700

    Added MySQL part

--- api/mySQL.py
+++ api/mySQL.py
@@ -2,3 +2,172 @@
 #            Rest API             #
 #     with mySQL as database      #
 # ------------------------------- #
+
+# Using mysql-connector-python
+# https://dev.mysql.com/doc/connector-python/en/
+
+import mysql.connector
+from mysql.connector import errorcode
+
+# from __future__ import print_function
+
+DB_NAME = "employees"
+
+TABLES = {}
+TABLES["employees"] = (
+    "CREATE TABLE `employees` ("
+    "  `emp_no` int NOT NULL AUTO_INCREMENT,"
+    "  `birth_date` date NOT NULL,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `gender` enum('M','F') NOT NULL,"
+    "  `hire_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`)"
+    ") ENGINE=InnoDB"
+)
+
+TABLES["departments"] = (
+    "CREATE TABLE `departments` ("
+    "  `dept_no` char(4) NOT NULL,"
+    "  `dept_name` varchar(40) NOT NULL,"
+    "  PRIMARY KEY (`dept_no`), UNIQUE KEY `dept_name` (`dept_name`)"
+    ") ENGINE=InnoDB"
+)
+
+TABLES["salaries"] = (
+    "CREATE TABLE `salaries` ("
+    "  `emp_no` int NOT NULL,"
+    "  `salary` int NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`,`from_date`), KEY `emp_no` (`emp_no`),"
+    "  CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`emp_no`) "
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB"
+)
+
+TABLES["dept_emp"] = (
+    "CREATE TABLE `dept_emp` ("
+    "  `emp_no` int NOT NULL,"
+    "  `dept_no` char(4) NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`,`dept_no`), KEY `emp_no` (`emp_no`),"
+    "  KEY `dept_no` (`dept_no`),"
+    "  CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_no`) "
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,"
+    "  CONSTRAINT `dept_emp_ibfk_2` FOREIGN KEY (`dept_no`) "
+    "     REFERENCES `departments` (`dept_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB"
+)
+
+TABLES["dept_manager"] = (
+    "  CREATE TABLE `dept_manager` ("
+    "  `emp_no` int NOT NULL,"
+    "  `dept_no` char(4) NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`,`dept_no`),"
+    "  KEY `emp_no` (`emp_no`),"
+    "  KEY `dept_no` (`dept_no`),"
+    "  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) "
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,"
+    "  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) "
+    "     REFERENCES `departments` (`dept_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB"
+)
+
+TABLES["titles"] = (
+    "CREATE TABLE `titles` ("
+    "  `emp_no` int NOT NULL,"
+    "  `title` varchar(50) NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date DEFAULT NULL,"
+    "  PRIMARY KEY (`emp_no`,`title`,`from_date`), KEY `emp_no` (`emp_no`),"
+    "  CONSTRAINT `titles_ibfk_1` FOREIGN KEY (`emp_no`)"
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB"
+)
+
+
+def create_database(cursor):
+    try:
+        cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'UTF8MB3'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+config = {
+    "user": "root",
+    "password": "SQL123456",
+    "host": "127.0.0.1",
+    "database": "block_flask",
+    "raise_on_warnings": True,
+}
+
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+        print("Creating table {}: ".format(table_name), end="")
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+cursor.close()
+cnx.close()
+
+
+# Using PyMySQL
+# https://pymysql.readthedocs.io/en/latest/
+# https://pymysql.readthedocs.io/en/latest/modules/cursors.html
+
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="SQL123456",
+    database="block_flask",
+    charset="utf8mb4",
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+with connection:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ("webmaster@python.org", "very-secret"))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ("webmaster@python.org",))
+        result = cursor.fetchone()
+        print(result)
